5222f3189eda15366388a7ccca25034527c3ad44
mekuanintlegese <mekuanint1212@gmail.com>
2023-10-27 16:02:25 +0300

    Static libraries

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - prints all arguments it receives.
+  *@argc: number of arguments received.
+  *@argv: array of size argc, containing all arguments received.
+  *
+  *Return: 0 on success.
+  */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
